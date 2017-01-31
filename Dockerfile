f2c448ac09bf6c8cfde2a513aaae35a85953a6eb
Antonis Kalou <kalouantonis@gmail.com>
2017-01-31 22:52:37 +0200

    Removed development dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM java:8-alpine
+
 MAINTAINER Antonis Kalou <kalouantonis@gmail.com>
 
-ADD target/uberjar/sound-app.jar /sound-app/app.jar
+ADD target/uberjar/sound-app.jar /usr/bin/sound-app.jar
 
 EXPOSE 3000
 
-CMD ["java", "-jar", "/sound-app/app.jar"]
+CMD ["java", "-jar", "/usr/bin/sound-app.jar"]
--- src/clj/sound_app/middleware.clj
+++ src/clj/sound_app/middleware.clj
@@ -2,6 +2,7 @@
   (:require [sound-app.env :refer [defaults]]
             [clojure.tools.logging :as log]
             [sound-app.layout :refer [*app-context* error-page]]
+            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
             [ring.middleware.webjars :refer [wrap-webjars]]
             [ring.middleware.format :refer [wrap-restful-format]]
             [sound-app.config :refer [env]]
@@ -33,6 +34,14 @@
                      :title "Something very bad has happened!"
                      :message "We've dispatched a team of highly trained gnomes to take care of the problem."})))))
 
+(defn wrap-csrf [handler]
+  (wrap-anti-forgery
+    handler
+    {:error-response
+     (error-page
+       {:status 403
+        :title "Invalid anti-forgery token"})}))
+
 (defn wrap-formats [handler]
   (let [wrapped (wrap-restful-format
                   handler
@@ -51,6 +60,5 @@
             ;; and wear tin-foil hat.
             (merge {:session false, :cookies false})
             (assoc-in [:security :anti-forgery] false)))
-      wrap-formats
       wrap-context
       wrap-internal-error))
