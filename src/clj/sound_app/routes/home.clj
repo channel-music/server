68669eb4b4170f2aa74f7577294ef2042cadf81c
Antonis Kalou <kalouantonis@gmail.com>
2017-02-05 15:56:04 +0200

    Re-organized commits and removed use of claudio

--- src/clj/sound_app/routes/home.clj
+++ src/clj/sound_app/routes/home.clj
@@ -1,12 +1,6 @@
 (ns sound-app.routes.home
   (:require [sound-app.layout :as layout]
-            [sound-app.db.core :as db]
-            [sound-app.validation :as v]
-            [compojure.core :refer [defroutes GET POST]]
-            [clojure.tools.logging :as log]
-            [clojure.java.io :as io]
-            [claudio.id3 :as id3]
-            [ring.util.response :refer [redirect]]))
+            [compojure.core :refer [defroutes GET]]))
 
 (defn home-page []
   (layout/render "home.html"))
