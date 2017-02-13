355d8f15033e3723fc89df89fcb84d105524ac0d
Antonis Kalou <kalouantonis@gmail.com>
2017-02-13 23:16:41 +0200

    Disallowed play queue from depleting by moving too far to the left

--- src/cljs/channel/play_queue.cljs
+++ src/cljs/channel/play_queue.cljs
@@ -10,12 +10,33 @@
       ;; Initialize zipper, we only have one level of nesting anyway.
       (z/down)))
 
+(defn first-track?
+  "Returns true if the play queue `pq` is pointing at the first track."
+  [pq]
+  (= pq (z/leftmost pq)))
+
+(defn last-track?
+  "Returns true if the play queue `pq` is pointing at the last track."
+  [pq]
+  (= pq (z/rightmost pq)))
+
 (def next-track
-  "Returns the play queue shifted one item to the right."
+  "Returns the play queue shifted one item to the right.
+  If there are no more items on the queue, nil is returned."
   z/right)
-(def previous-track
-  "Returns the play queue shifted one item to the left."
-  z/left)
+
+;; We return nil for `next-track` because it makes sense for the
+;; play queue to be depleted once listened to. However, moving too
+;; far back in the queue shouldn't remove all tracks.
+(defn previous-track
+  "Returns the play queue shifted one item to the left.
+  If there are no more items to the left, remains on the
+  leftmost item."
+  [pq]
+  (if (first-track? pq)
+    pq
+    (z/left pq)))
+
 (def track-id
   "Returns the ID of the song in the current play queue."
   z/node)
