b6ff6db8d70e4dc24be625e8f89dfdcbe62257e6
Antonis Kalou <kalouantonis@gmail.com>
2017-02-25 13:02:52 +0000

    Changed on-ended watcher to use the builtin audio events

--- src/cljs/channel/audio.cljs
+++ src/cljs/channel/audio.cljs
@@ -1,26 +1,11 @@
 (ns channel.audio
-  "Clojure interface to HTML5 Audio"
-  (:require [cljs.core.async :refer [<!] :as async])
-  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
+  "Clojure interface to HTML5 Audio")
 
 ;; TODO: Figure out how to "synchronize" browser audio state
 ;; TODO: and play-queue state OR try and figure out how to provide
 ;; TODO: a functional-like interface on top of stateful objects.
 ;;
 ;; Perhaps this can also be considered a component
-
-(defn- watch-on-ended
-  "Calls the function `f` with `audio` once `audio` has ended. Takes a `pause-ms`
-  value that represents the amount of time (in ms) to wait before checking the ended
-  status again (defaults to 500ms)."
-  ([audio f] (watch-on-ended audio f 500))
-  ([audio f pause-ms]
-   (go-loop []
-     (if (.-ended audio)
-       (f audio)
-       (do
-         (<! (async/timeout pause-ms))
-         (recur))))))
 
 (defn make-audio
   "Create a new `js/Audio` object using song data."
@@ -29,10 +14,13 @@
    (let [audio (js/Audio. (:file song))]
      ;; Setup callbacks
      (when on-ended
-       (watch-on-ended audio on-ended))
+       (set! (.-onended audio) #(on-ended audio)))
      audio)))
 
 (def ^:private current-audio (atom nil))
+
+(defn muted? []
+  (.muted @current-audio))
 
 (defn pause! []
   (when-let [audio @current-audio]
@@ -43,6 +31,3 @@
   ([audio]
    (reset! current-audio audio)
    (.play audio)))
-
-(defn muted? []
-  (.muted @current-audio))
