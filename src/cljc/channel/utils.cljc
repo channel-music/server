bf1c19d3930aa6b07a79eb4e73c62d5c67fdff26
Antonis Kalou <kalouantonis@gmail.com>
2017-02-25 19:26:24 +0000

    Passed util tests

--- src/cljc/channel/utils.cljc
+++ src/cljc/channel/utils.cljc
@@ -7,14 +7,15 @@
 (defn any-nil?
   "Returns true if any items in collection `coll` are nil."
   [coll]
-  (some (comp not nil?) coll))
+  (some nil? coll))
 
 (defn maybe
   "Returns a function that calls `f` with all parameters, given
   that one or more parameters are not nil."
   [f]
   (fn [& args]
-    (when (any-nil? args)
+    ;; Call f if there are no args
+    (when-not (and (seq args) (any-nil? args))
       (apply f args))))
 
 ;;
--- test/clj/channel/test/utils.clj
+++ test/clj/channel/test/utils.clj
@@ -5,8 +5,8 @@
 (deftest any-nil?-test
   (testing "true for a collection of nil values"
     (is (any-nil? [nil nil nil])))
-  (testing "true for a plain nil value"
-    (is (any-nil? nil)))
+  (testing "false for a plain nil value"
+    (is (not (any-nil? nil))))
   (testing "false for a collection of non-nil values"
     (is (not (any-nil? [1 2 3 4]))))
   (testing "false for a collection containing at least one non-nil value"
@@ -19,13 +19,12 @@
   (let [f (maybe (fn [& args] args))]
     (testing "calls function if no params are nil"
       (is (= (f 1 2 3) [1 2 3])))
-    (testing "does't call function if any param is nil"
+    (testing "doesn't call function if any param is nil"
       (are [coll] (= (apply f coll) nil)
         [1 2 nil 4]
         [1 nil nil 4]
         [nil nil nil 4])))
-  (let [called (atom false)
-        f (maybe (fn [& _] (reset! called true)))]
+  (let [f (maybe (fn [& _] :called))]
     (testing "calls function if there are no parameters"
-      (is (= (f) nil))
+      (is (= (f) :called))
       (is (true? @called)))))
