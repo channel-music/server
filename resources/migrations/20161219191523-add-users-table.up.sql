a4d4c6ea59fe6038325fcf2b17f6d77b34f784c9
Antonis Kalou <kalouantonis@gmail.com>
2017-03-14 20:26:42 +0200

    Added is_admin field to user table

--- resources/migrations/20161219191523-add-users-table.up.sql
+++ resources/migrations/20161219191523-add-users-table.up.sql
@@ -2,7 +2,8 @@ CREATE TABLE users
 (id       SERIAL       NOT NULL PRIMARY KEY,
  username VARCHAR(30)  UNIQUE NOT NULL,
  email    VARCHAR(30),
- password VARCHAR(100) NOT NULL);
+ password VARCHAR(100) NOT NULL
+ is_admin BOOLEAN      NOT NULL DEFAULT false);
 
 ---
 --- username is always queried
