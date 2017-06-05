60438f65aa6da6bf8bb452621e6f672240d71cc3
Antonis Kalou <kalouantonis@gmail.com>
2017-06-05 17:05:03 +0300

    Initial commit

---
+++ src/main/kotlin/kalouantonis/channel/Application.kt
@@ -0,0 +1,11 @@
+package kalouantonis.channel
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
---
+++ src/main/kotlin/kalouantonis/channel/Song.kt
@@ -0,0 +1,55 @@
+package kalouantonis.channel
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class Song(val title: String,
+                val artist: String,
+                val album: String) {
+    constructor(): this("", "", "")
+
+    @Id
+    @GeneratedValue
+    var id: Long? = null
+}
+
+
+interface SongRepository : JpaRepository<Song, Long>
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class SongNotFoundException(songId: Long) : RuntimeException("could not find song '$songId'")
+
+@RestController
+@RequestMapping("/songs")
+class SongRestController(val songRepository: SongRepository) {
+
+    @RequestMapping(method = arrayOf(RequestMethod.GET))
+    fun allSongs(): Collection<Song> = songRepository.findAll()
+
+    @RequestMapping(method = arrayOf(RequestMethod.GET), value = "/{songId}")
+    fun songById(@PathVariable songId: Long): Song? =
+        songRepository.findById(songId).orElseThrow { SongNotFoundException(songId) }
+
+    @RequestMapping(method = arrayOf(RequestMethod.POST))
+    fun createSong(@RequestBody song: Song): ResponseEntity<Any> {
+        val result = songRepository.save(song)
+        val location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(result.id).toUri()
+        return ResponseEntity.created(location).build()
+    }
+
+    @RequestMapping(method = arrayOf(RequestMethod.DELETE), value = "/{songId}")
+    fun deleteSong(@PathVariable songId: Long): ResponseEntity<Any> {
+        songRepository.deleteById(songId)
+        return ResponseEntity.noContent().build()
+    }
+
+}
