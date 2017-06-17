374aba37facfb1a93a0bd76fd562c00b88f7b323
Antonis Kalou <kalouantonis@protonmail.com>
2017-06-17 12:22:18 +0300

    Added additional tests for temp-file expiry

--- test/channel/upload/temp_file_store_test.clj
+++ test/channel/upload/temp_file_store_test.clj
@@ -1,4 +1,6 @@
 (ns channel.upload.temp-file-store-test
+  "Tests for channel.upload.temp-file-store. These tests are nearly
+  identical to those provided for a similar store in ring-core."
   (:require
    [channel.upload.temp-file-store :refer [temp-file-store]]
    [clojure.test :refer :all]
@@ -23,4 +25,39 @@
     (is (= (slurp (:tempfile result)) "foo"))))
 
 
-;; TODO: test cleanup
+(defn eventually [check n delay]
+  (loop [i n]
+    (if (check)
+      true
+      (when (pos? i)
+        (Thread/sleep delay)
+        (recur (dec i))))))
+
+
+(deftest test-temp-file-expiry
+  (let [store (temp-file-store {:expires-in 2})
+        result (store
+                {:filename "foo.txt"
+                 :content-type "text/plain"
+                 :stream (string-input-stream "foo")})]
+    (is (.exists (:tempfile result)))
+    (Thread/sleep 2000)
+    (let [deleted? (eventually #(not (.exists (:tempfile result))) 120 250)]
+      (is deleted?))))
+
+
+(defn thread-stacktrace []
+  (.keySet (Thread/getAllStackTraces)))
+
+
+(deftest test-temp-file-threads
+  (let [threads0 (thread-stacktrace)
+        store (temp-file-store)
+        threads1 (thread-stacktrace)]
+    (is (= (count threads0)
+           (count threads1)))
+    (dotimes [_ 200]
+      (store {:filename "foo.txt"
+              :content-type "text/plain"
+              :stream (string-input-stream "foo")}))
+    (is (< (count (thread-stacktrace)) 100))))
