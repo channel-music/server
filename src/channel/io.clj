2bb86f1802ade95bcd6409fcdc1075ac7f934dc9
Antonis Kalou <kalouantonis@protonmail.com>
2017-07-01 19:48:38 +0300

    Add `delete` IO utility function
    This function deletes files and folders alike, regardless of whether
    the folder is empty or not.

--- src/channel/io.clj
+++ src/channel/io.clj
@@ -1,6 +1,8 @@
 (ns channel.io
   (:require [clojure.java.io :as io])
-  (:import (java.nio.file Paths Path)))
+  (:import
+   (java.nio.file Paths Path)
+   (org.apache.commons.io FileUtils)))
 
 
 (defn tmpdir
@@ -20,3 +22,17 @@
   "Convert a plain string to a java `Path` object."
   ^Path [^String s]
   (Paths/get (.toURI (io/file s))))
+
+
+(defn delete
+  "Delete a file or directory. If the directory is not empty it
+  will be deleted regardless. Returns `true` on success and `false`
+  on failure."
+  [file]
+  (if (.isDirectory file)
+    (try
+      (FileUtils/deleteDirectory file)
+      true
+      (catch IllegalArgumentException e
+        false))
+    (.delete file)))
--- test/channel/io_test.clj
+++ test/channel/io_test.clj
@@ -1,6 +1,7 @@
 (ns channel.io-test
   (:require
    [channel.io :as io]
+   [clojure.java.io]
    [clojure.test :refer :all]))
 
 
@@ -33,3 +34,30 @@
     (let [path (io/str->path "src/")]
       (is (instance? java.nio.file.Path path))
       (is (= "src" (.toString (.getFileName path)))))))
+
+
+(deftest test-delete
+  (testing "with a plain file"
+    (spit "plain-file.txt" "abcde")
+    (let [f (clojure.java.io/file "plain-file.txt")]
+      (is (.exists f))
+      (is (io/delete f))
+      (is (not (.exists f)))))
+
+  (testing "with an empty directory"
+    (let [dir (clojure.java.io/file "test-dir")]
+      (.mkdir dir)
+      (is (.exists dir))
+      (is (io/delete dir))
+      (is (not (.exists dir)))))
+
+  (testing "with a non-empty directory"
+    (let [dir (clojure.java.io/file "test-dir")]
+      (.mkdir dir)
+      (spit (io/path-join "test-dir" "file1.txt") "file1")
+      (spit (io/path-join "test-dir" "file2.txt") "file2")
+      (is (io/delete dir))
+      (is (not (.exists dir)))))
+
+  (testing "with a non-existent directory"
+    (is (not (io/delete (clojure.java.io/file "doesnt-exist"))))))
